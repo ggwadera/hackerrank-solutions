c3a943da3d278059418d1d243133556b40aaee3f
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-06-26 15:20:22 -0300

    solution for day 8/30

---
+++ Java/src/com/thirtydaysofcode/solutions/DictionariesAndMaps.java
@@ -0,0 +1,36 @@
+/**
+ * DAY 8/30
+ * Dictionaries and Maps
+ * https://www.hackerrank.com/challenges/30-dictionaries-and-maps/
+ */
+
+package com.thirtydaysofcode.solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DictionariesAndMaps {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Map<String, Integer> phoneBook = new HashMap<>();
+
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.putIfAbsent(name, phone);
+        }
+        while (in.hasNext()) {
+            String s = in.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+
+}
